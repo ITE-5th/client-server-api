b8daffc10f9eb12234b6d897e70bd542bef2b1f7
zaherairout <zaher2995>
2018-03-11 08:14:53 +0200

    fix client server connection

---
+++ client/client.py
@@ -0,0 +1,66 @@
+import json
+import socket
+
+from TTS import TTS
+
+
+# from client.camera import Camera
+
+
+class Client:
+    def __init__(self, host=socket.gethostname(), port=1234):
+        self.host = host
+        self.port = port
+        # self.cam = Camera()
+        self.tts = TTS()
+        # self.recognizer = Recognizer(server=self)
+
+        # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # connect the client
+        # client.connect((target, port))
+
+    def start(self):
+        print('connected to server ' + self.host + ':' + str(self.port))
+
+        self.socket.connect((self.host, self.port))
+        self.send()
+        self.close()
+        # #     start recogniser
+        # self.recognizer.start()
+
+    def close(self):
+        self.socket.close()
+
+    def send(self):
+        self.socket.send(self._build_message('caption'))
+        response = self.socket.recv(4096)
+        response = json.loads(response)
+
+        self.tts.say(response['result'])
+        self.close()
+
+    def _build_message(self, type, question=None):
+        # type == "visual-question-answering"
+        # type == "face-recognition"
+        # type == "image-to-text"
+
+        if question is None:
+            return json.dumps(
+                {
+                    "type": type,
+                    "image": "",
+                    # "image": self.cam.take_image(),
+                }).encode()
+        return json.dumps({
+            "type": "visual-question-answering",
+            "image": "",
+            # "image": self.cam.take_image(),
+            "question": question,
+        }).encode()
+
+
+if __name__ == '__main__':
+    api = Client()
+    api.start()
--- server.py
+++ server.py
@@ -1,43 +1,55 @@
-import base64
 import json
 import socket
 
-from face_recognition.face_recognition import FaceRecognition
-from image_to_text.image_to_text import ImageToText
-from vqa.vqa import Vqa
+# from face_recognition.face_recognition import FaceRecognition
+# from image_to_text.image_to_text import ImageToText
+# from vqa.vqa import Vqa
+import threading
 
 
 class Server:
-    def __init__(self, host=socket.gethostname(), port=8888):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def __init__(self, host=socket.gethostname(), port=1234):
+        self.host = host
+        self.port = port
+        # self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((host, port))
-        self.vqa = Vqa()
-        self.face_recognition = FaceRecognition()
-        self.image_to_text = ImageToText()
+        self.socket.listen(5)
+        # self.vqa = Vqa()
+        # self.face_recognition = FaceRecognition()
+        # self.image_to_text = ImageToText()
+
+        self.client_socket, self.address = None, None
+
+    def handle_client_connection(self, client_socket):
+        message = client_socket.recv(100000)
+        message = json.loads(message)
+        print(message)
+        type = message["type"].lower()
+        # image = message["image"]
+        # image = base64.decodebytes(image)
+        result = {"result": 'test'}
+        if type == "visual-question-answering":
+            question = message["question"]
+            result["result"] = self.vqa.predict(question, image)
+        elif type == "face-recognition":
+            result["result"] = self.face_recognition.predict(image)
+        elif type == "image-to-text":
+            result["result"] = self.image_to_text.predict(image)
+        result = json.dumps(result)
+        client_socket.send(result.encode())
 
     def start(self):
+        print('server started at {}:{}'.format(self.host, str(self.port)))
         while True:
-            message, _ = self.receive()
-            message = json.loads(message)
-            type = message["type"].lower()
-            image = message["image"]
-            image = base64.decodebytes(image)
-            result = {}
-            if type == "visual-question-answering":
-                question = message["question"]
-                result["result"] = self.vqa.predict(question, image)
-            elif type == "face-recognition":
-                result["result"] = self.face_recognition.predict(image)
-            elif type == "image-to-text":
-                result["result"] = self.image_to_text.predict(image)
-            result = json.dumps(result)
-            self.send(result)
+            client_socket, address = self.socket.accept()
+            print('Accepted connection from {}:{}'.format(address[0], address[1]))
 
-    def receive(self):
-        return self.socket.recv(100000)
-
-    def send(self, result):
-        self.socket.send(result)
+            client_handler = threading.Thread(
+                target=self.handle_client_connection,
+                args=(client_socket,)
+            )
+            client_handler.start()
 
     def close(self):
         self.socket.close()
@@ -47,5 +59,7 @@ if __name__ == '__main__':
     server = Server()
     try:
         server.start()
+    except:
+        print('server stopped')
     finally:
         server.close()
